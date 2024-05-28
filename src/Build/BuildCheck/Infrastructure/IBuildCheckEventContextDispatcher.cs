d86802026d10e8d319247f487504e321a90ff940
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2024-05-28 17:27:00 +0200

    merge from main

---
+++ src/Build/BuildCheck/Infrastructure/IBuildCheckEventContextDispatcher.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Shared;
+
+namespace Microsoft.Build.Experimental.BuildCheck.Infrastructure;
+
+internal interface IBuildCheckEventContextDispatcher
+{
+    void DispatchAsComment(BuildEventContext buildEventContext, MessageImportance importance, string messageResourceName, params object[] messageArgs);
+
+    void DispatchAsBuildEvent(BuildEventArgs buildEvent);
+
+    void DispatchAsErrorFromText(BuildEventContext buildEventContext, string? subcategoryResourceName, string? errorCode, string? helpKeyword, BuildEventFileInfo file, string message);
+
+    void DispatchAsCommentFromText(BuildEventContext buildEventContext, MessageImportance importance, string message);
+}
---
+++ src/Build/BuildCheck/Infrastructure/LoggingDispatcher.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.BackEnd.Logging;
+using Microsoft.Build.Experimental.BuildCheck.Infrastructure;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Shared;
+
+namespace Microsoft.Build.Experimental.BuildCheck.Infrastructure;
+
+internal class LoggingDispatcher : IBuildCheckEventContextDispatcher
+{
+    private ILoggingService _loggingService;
+
+    public LoggingDispatcher(ILoggingService loggingService)
+    {
+        _loggingService = loggingService;
+    }
+
+    public void DispatchAsComment(
+        BuildEventContext buildEventContext,
+        MessageImportance importance,
+        string messageResourceName,
+        params object[] messageArgs)
+        => _loggingService.LogComment(buildEventContext, importance, messageResourceName, messageArgs);
+
+    public void DispatchAsCommentFromText(BuildEventContext buildEventContext, MessageImportance importance, string message)
+        => _loggingService.LogCommentFromText(buildEventContext, importance, message);
+
+    public void DispatchAsErrorFromText(
+        BuildEventContext buildEventContext,
+        string subcategoryResourceName,
+        string errorCode,
+        string helpKeyword,
+        BuildEventFileInfo file,
+        string message)
+        => _loggingService.LogErrorFromText(buildEventContext, subcategoryResourceName, errorCode, helpKeyword, file, message);
+
+    public void DispatchAsBuildEvent(BuildEventArgs buildEvent)
+        => _loggingService.LogBuildEvent(buildEvent);
+}
